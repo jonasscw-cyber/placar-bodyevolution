36f2c9fa93d530e6b01e0d352cec26b786fcb289
jonasscw-cyber <jonas.scw@gmail.com>
2026-06-02 07:56:18 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+ BIN_ID:    6a1eac71f5f4af5e29ac62ed
+  READ_KEY:  $2a$10$TIsg4Q0rkhLM.nR940AXN.xTcQImy0rcN/fqfRqTJk3Ycr2EqZ/1W
+  WRITE_KEY: $2a$10$lH3zoFQ7Z17nkThST2EXD.7K.AJoAWfh.xSGnwiBnuirFXdcbOA0u
+  ADMIN_PASS: BodyEvo@2026
