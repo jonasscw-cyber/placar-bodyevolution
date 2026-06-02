e62f30196cab61f17d30fa18640e399a60387514
jonasscw-cyber <jonas.scw@gmail.com>
2026-06-02 08:24:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
- BIN_ID:    6a1eac71f5f4af5e29ac62ed
-  READ_KEY:  $2a$10$TIsg4Q0rkhLM.nR940AXN.xTcQImy0rcN/fqfRqTJk3Ycr2EqZ/1W
-  WRITE_KEY: $2a$10$lH3zoFQ7Z17nkThST2EXD.7K.AJoAWfh.xSGnwiBnuirFXdcbOA0u
-  ADMIN_PASS: BodyEvo@2026
+const CONFIG = {
+  BIN_ID: "6a1eac71f5f4af5e29ac62ed",
+  READ_KEY: "$2a$10$TISg4Q0rkhLM.nR940AXN.xTcQImy0rcN/fqfRqTJk3Ycr2EqZ/1W",
+  WRITE_KEY: "$2a$10$1H3zoFQ7Z17nkThST2EXD.7K.AJoAWfh.xSGnwiBnuirFXdcbOA0u",
+  ADMIN_PASS: "BodyEvo@2026"
+};
